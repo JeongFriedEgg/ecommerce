d6108fe59b9b4d335d2316c8a41417b5c4f0d053
jeongbanseog <wibalislfilask@gmail.com>
2025-04-30 13:01:31 +0900

    feat : 상품 기능에 대한 예외처리

---
+++ src/main/java/com/market/ecommerce/exception/product/ProductErrorCode.java
@@ -0,0 +1,20 @@
+package com.market.ecommerce.exception.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ProductErrorCode {
+    INVALID_CATEGORY(HttpStatus.BAD_REQUEST,"유효하지 않은 카테고리입니다."),
+    INVALID_PRODUCT_STATUS(HttpStatus.BAD_REQUEST,"유효하지 않은 상품 상태입니다."),
+    ;
+
+    private final HttpStatus status;
+    private final String message;
+
+    public int getStatusCode() {
+        return this.status.value();
+    }
+}
